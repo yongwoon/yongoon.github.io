18256c07e2f8d0fb6e64987ebae7b782f05fda42
yongwoon <yonyon.japan@gmail.com>
2021-08-19 17:43:06 +0900

    tmp

--- docs/packages.md
+++ docs/packages.md
@@ -2,4 +2,8 @@
 
 ```bash
 npm install next-optimized-images
+
+next-optimized-images imagemin-mozjpeg imagemin-pngquant webp-loader responsive-loader sharp raw-loader
 ```
+
+https://zenn.dev/itomise/articles/20356a794b665f
--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+// next.config.js
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+
+};
+
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      /* config for next-optimized-images */
+    },
+      reactStrictMode: true,
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
-};
+  ],
+  
+
+  // your other plugins here
+]);
