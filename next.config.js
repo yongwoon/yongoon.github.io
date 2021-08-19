4857a37083d344232c72027867d7ec0027391fd7
yongwoon <yonyon.japan@gmail.com>
2021-08-19 16:37:11 +0900

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+};
